dc8684d7e073cc44b3b5669cb3da5bd961c2407b
Zijian Peng <pengzijian@gmail.com>
2021-05-26 17:32:47 +0800

    unique email and generated public id

--- src/main/java/com/zijian/java/web/spring/webapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/zijian/java/web/spring/webapp/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.zijian.java.web.spring.webapp.service.impl;
 import com.zijian.java.web.spring.webapp.UserRepository;
 import com.zijian.java.web.spring.webapp.io.entity.UserEntity;
 import com.zijian.java.web.spring.webapp.service.UserService;
+import com.zijian.java.web.spring.webapp.shared.Utils;
 import com.zijian.java.web.spring.webapp.shared.dto.UserDto;
 
 import org.springframework.beans.BeanUtils;
@@ -15,14 +16,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
-        
+
+        if(userRepository.findUserByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists.");
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId();
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("123");
 
         UserEntity storedUser = userRepository.save(userEntity);
 
---
+++ src/main/java/com/zijian/java/web/spring/webapp/shared/Utils.java
@@ -0,0 +1,27 @@
+package com.zijian.java.web.spring.webapp.shared;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+
+    private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public String generateUserId() {
+        return generateRandomString(20);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder result = new StringBuilder(length);
+
+        for (int i =0; i<length; i++){
+            result.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+
+        return new String(result);
+    }
+}
