d6d8c34ad1ba64080e1b099133a71e7c933723f9
Zijian Peng <pengzijian@gmail.com>
2021-06-02 15:33:24 +0800

    build war

--- src/main/java/com/zijian/java/web/spring/webapp/WebAppApplication.java
+++ src/main/java/com/zijian/java/web/spring/webapp/WebAppApplication.java
@@ -4,11 +4,17 @@ import com.zijian.java.web.spring.webapp.security.AppProperties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class WebAppApplication {
+public class WebAppApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
